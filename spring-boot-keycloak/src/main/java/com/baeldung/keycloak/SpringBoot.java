1322cb109c2fee4908b55f7fb87ffa7692fe39d3
spafka <spafka@163.com>
2021-06-02 18:56:10 +0800

    init

--- spring-boot-keycloak/src/main/java/com/baeldung/keycloak/SpringBoot.java
+++ spring-boot-keycloak/src/main/java/com/baeldung/keycloak/SpringBoot.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
-@EnableWebSecurity(debug = false)
+@EnableWebSecurity(debug = true)
 public class SpringBoot {
 
     public static void main(String[] args) {
