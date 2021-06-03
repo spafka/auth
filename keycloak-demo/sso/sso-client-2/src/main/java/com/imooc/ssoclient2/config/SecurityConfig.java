e1c79a3af664f7b436d172293dbd98411446b433
spafka <spafka@163.com>
2021-06-03 18:45:32 +0800

    custom keycloak provider

--- keycloak-demo/sso/sso-client-2/src/main/java/com/imooc/ssoclient2/config/SecurityConfig.java
+++ keycloak-demo/sso/sso-client-2/src/main/java/com/imooc/ssoclient2/config/SecurityConfig.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.WebClient;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
